19ca02e943a7bea9e7cf70bf43f24d9d4b86d6e9
BugbearR <bugbearr@bugbearr.jp>
2025-07-06 03:05:34 +0000

    Add raw argument printing to main function

--- args/args/src/main.rs
+++ args/args/src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 
 fn main() {
+    let mut args_raw = env::args();
+    println!("{:?}", args_raw);
+    println!("{:?}", args_raw.next());
+    println!("{:?}", args_raw.next());
+    println!("{:?}", args_raw.next());
+
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
